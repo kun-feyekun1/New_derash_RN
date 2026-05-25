5b9dc016bea1136c0e7cd2ec4ab025b22107d12b
Mohammed Hassen Ahmed <mohammed.ugr-2340-16@aau.edu.et>
2026-05-25 09:37:42 -0200

    metro added

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,7 @@
-const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require('nativewind/metro');
+ 
+const config = getDefaultConfig(__dirname)
+ 
+module.exports = withNativeWind(config, { input: './global.css' })
